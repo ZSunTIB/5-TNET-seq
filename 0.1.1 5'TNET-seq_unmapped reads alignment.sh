641aa76dcc96c70a5fcf4509b472192474f9414e
ZSunTIB <151911460+ZSunTIB@users.noreply.github.com>
2023-12-01 16:35:42 +0800

    Add files via upload

---
+++ 0.1.1 5'TNET-seq_unmapped reads alignment.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+##########################################################################################################
+fq_R2=Raw_fastq_R2
+##########################################################################################################
+#Extract all unmapped reads
+bowtie /Users/sz/Data_analysis/Genome/Bowtie_genome/MG1655_v2 -q \
+$fq_R2\_R12_cutadapt_remD_6Ntrim_remEM.fastq \
+-S $fq_R2\_R12_cutadapt_remD_6Ntrim_remEM_multiple.sam
+
+samtools sort $fq_R2\_R12_cutadapt_remD_6Ntrim_remEM_multiple.sam -o \
+$fq_R2\_R12_multiple_sort.sam
+
+mkdir Processed_5TNET_R21_unmapped
+
+#Remove all mapped reads
+samtools view -f 4 $fq_R2\_R12_multiple_sort.sam -b -o ./Processed_5TNET_R21_unmapped/$fq_R2\_R12_unmapped.bam
+
+#convert .bam file to .fastq file.
+bedtools bamtofastq -i ./Processed_5TNET_R21_unmapped/$fq_R2\_R12_unmapped.bam -fq ./Processed_5TNET_R21_unmapped/$fq_R2\_R12_unmapped.fastq
+
+rm $fq_R2\_R12_cutadapt_remD_6Ntrim_remEM_multiple.sam $fq_R2\_R12_multiple_sort.sam
+#rm $fq_R2\_R12_cutadapt_remD_6Ntrim_remEM.fastq
